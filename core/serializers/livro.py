368ad38284e45f3fc8e4e09a27c83853167e196d
asantosmanu <manuelasantos452006@gmail.com>
2024-09-09 11:44:19 -0300

    finalizando 12

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,4 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Livro
 
@@ -6,6 +9,18 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
+class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(
+        required=False,
+        read_only=True
+    )
 
 
 class LivroDetailSerializer(ModelSerializer):
